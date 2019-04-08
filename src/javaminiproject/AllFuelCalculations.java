1e24347b34209e7d6b14e4ab04379ba1e88e252d
Aditeya <22963960+Aditeya@users.noreply.github.com>
2019-04-08 16:56:11 +0400

    Added ArrayList to new subclass

--- src/javaminiproject/AllFuelCalculations.java
+++ src/javaminiproject/AllFuelCalculations.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaminiproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author adite
+ * This class is used to store FuelCalculation objects in an ArrayList
+ * @author Aditeya Viju Govind
  */
-public class AllFuelCalculations {
+public class AllFuelCalculations extends FuelCalculation {
+
+    private final List<FuelCalculation> Calculations;
+
+    public AllFuelCalculations() {
+        Calculations = new ArrayList<>();
+    }
+
+    public void add(FuelCalculation fc){
+        Calculations.add(fc);
+    }
     
+    @Override
+    public String toString() {
+        StringBuilder toString = new StringBuilder();
+        
+        for(FuelCalculation f:Calculations){
+            toString.append(f);
+            toString.append("\n");
+        }
+        
+        return toString.toString();
+    }
+
 }
